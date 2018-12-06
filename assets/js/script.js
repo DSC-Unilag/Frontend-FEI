679ead28c14ad5f8a4f5883664d7438cce7e320c
Mikaelson1101 <eugenemichael1999@gmail.com>
2018-12-06 21:06:44 +0100

    News UI

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Navigation Bar
+const navToggle = document.querySelector('.menu-icon')
+
+navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-is-visible")
+})
+
+// Box Shadow
+// window.onscroll = function() {
+//     myFunction()
+// }
+
+// let header = document.getElementById('fixed')
+// let sticky = header.offsetTop
+
+// function myFunction() {
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add('fixed-header')
+//     }   else {
+//         header.classList.remove('fixed-header')
+//     }
+// }
