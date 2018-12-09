e5a1d4fee98eeccf60216ed206086a9861c3b5a9
Mikaelson1101 <eugenemichael1999@gmail.com>
2018-12-09 13:55:53 +0100

    Need to Improve Loop Function in js

--- michael-eugene/assets/js/script.js
+++ michael-eugene/assets/js/script.js
@@ -5,138 +5,93 @@ navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-is-visible")
 })
 
+
+var numbers_ = [1, 2, 3, 4, 5];
+var text = "";
+var i;
+for (i = 0; i < numbers_.length; i++) { 
+  text += numbers_[i] - 1
+}
+
+
 // BBC API
 var url = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=f3d6aa29b4af4d45ad9cb6542b496c04";
 
 fetch(url)
     .then((response) => response.json())
-    .then((data) => 
+    .then((response) => 
 
-        document.getElementsByClassName('wrapper')[0].innerHTML = `
-        <div class="grid-item">
-            <div class="img-wrapper"><img src="${data.articles[0].urlToImage}"></div>
+        response.articles.slice(-4).forEach(art => {
+            document.getElementsByClassName('wrapper')[0].innerHTML += `
+        <div class="grid-item sm">
+            <div class="img-wrapper"><img src="${response.articles[0].urlToImage}"></div>
             <div class="hero-txt">
-                <h2 class="grid-txt">${data.articles[0].title}</h2>
-                <p class="grid-txt">${data.articles[0].description}</p>
-                <div><a href="${data.articles[0].url}" class="btn-sec">Read More</a></div>
+                <h2 class="grid-txt">${response.articles[0].title}</h2>
+                <p class="grid-txt">${response.articles[0].description}</p>
+                <div><a href="${response.articles[0].url}" class="btn-sec">Read More</a></div>
             </div>
         </div>
-        `);
+        `
+        }))
 
 // Technology API
 var url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=f3d6aa29b4af4d45ad9cb6542b496c04";
 
 fetch(url)
     .then((response) => response.json())
-    .then((data) => 
+    .then((response) => 
 
-        document.getElementsByClassName('wrapper')[1].innerHTML = `
-        <div class="grid-item">
-            <div class="img-wrapper"><img src="${data.articles[0].urlToImage}"></div>
+        response.articles.slice(-4).forEach(art => {
+            document.getElementsByClassName('wrapper')[1].innerHTML += `
+        <div class="grid-item sm">
+            <div class="img-wrapper"><img src="${response.articles[0].urlToImage}"></div>
             <div class="hero-txt">
-                <h2 class="grid-txt">${data.articles[0].title}</h2>
-                <p class="grid-txt">${data.articles[0].description}</p>
-                <div><a href="${data.articles[0].url}" class="btn-sec">Read More</a></div>
+                <h2 class="grid-txt">${response.articles[0].title}</h2>
+                <p class="grid-txt">${response.articles[0].description}</p>
+                <div><a href="${response.articles[0].url}" class="btn-sec">Read More</a></div>
             </div>
         </div>
-        `);
+        `
+        }))
 
 // Entertainment API
 var url = "https://newsapi.org/v2/top-headlines?sources=mtv-news&apiKey=f3d6aa29b4af4d45ad9cb6542b496c04";
 
 fetch(url)
     .then((response) => response.json())
-    .then((data) => 
+    .then((response) => 
 
-        document.getElementsByClassName('wrapper')[2].innerHTML = `
-        <div class="grid-item">
-            <div class="img-wrapper"><img src="${data.articles[0].urlToImage}"></div>
+        response.articles.slice(-4).forEach(art => {
+            document.getElementsByClassName('wrapper')[2].innerHTML += `
+        <div class="grid-item sm">
+            <div class="img-wrapper"><img src="${response.articles[0].urlToImage}"></div>
             <div class="hero-txt">
-                <h2 class="grid-txt">${data.articles[0].title}</h2>
-                <p class="grid-txt">${data.articles[0].description}</p>
-                <div><a href="${data.articles[0].url}" class="btn-sec">Read More</a></div>
+                <h2 class="grid-txt">${response.articles[0].title}</h2>
+                <p class="grid-txt">${response.articles[0].description}</p>
+                <div><a href="${response.articles[0].url}" class="btn-sec">Read More</a></div>
             </div>
         </div>
-        `);
+        `
+        }))
 
 // Sport API
 var url = "https://newsapi.org/v2/top-headlines?sources=espn&apiKey=f3d6aa29b4af4d45ad9cb6542b496c04";
 
 fetch(url)
     .then((response) => response.json())
-    .then((data) => 
+    .then((response) => 
 
-        document.getElementsByClassName('wrapper')[3].innerHTML = `
-        <div class="grid-item">
-            <div class="img-wrapper"><img src="${data.articles[0].urlToImage}"></div>
+        response.articles.slice(-4).forEach(art => {
+            document.getElementsByClassName('wrapper')[3].innerHTML += `
+        <div class="grid-item sm">
+            <div class="img-wrapper"><img src="${response.articles[0].urlToImage}"></div>
             <div class="hero-txt">
-                <h2 class="grid-txt">${data.articles[0].title}</h2>
-                <p class="grid-txt">${data.articles[0].description}</p>
-                <div><a href="${data.articles[0].url}" class="btn-sec">Read More</a></div>
+                <h2 class="grid-txt">${response.articles[0].title}</h2>
+                <p class="grid-txt">${response.articles[0].description}</p>
+                <div><a href="${response.articles[0].url}" class="btn-sec">Read More</a></div>
             </div>
         </div>
-        `);
-
-        
-        // fetch(url)
-        // .then((response) => response.json())
-        // .then((data) => 
-    
-        //     document.getElementsByClassName('js-item')[1].innerHTML = `
-        //     <div class="img-wrapper"></div>
-        //     <div>
-        //         <h3 class="grid-txt">${data.articles[1].title}</h3>
-        //         <div><a href="${data.articles[1].url}" class="btn-sec">Read More</a></div>
-        //     </div>
-        //     `);
-
-        // fetch(url)
-        // .then((response) => response.json())
-        // .then((data) => 
-    
-        //     document.getElementsByClassName('js-item')[2].innerHTML = `
-        //     <div class="img-wrapper"></div>
-        //     <div>
-        //         <h3 class="grid-txt">${data.articles[2].title}</h3>
-        //         <div><a href="${data.articles[2].url}" class="btn-sec">Read More</a></div>
-        //     </div>
-        //     `);
-
-        // fetch(url)
-        // .then((response) => response.json())
-        // .then((data) => 
-    
-        //     document.getElementsByClassName('js-item')[3].innerHTML = `
-        //     <div class="img-wrapper"></div>
-        //     <div>
-        //         <h3 class="grid-txt">${data.articles[3].title}</h3>
-        //         <div><a href="${data.articles[3].url}" class="btn-sec">Read More</a></div>
-        //     </div>
-        //     `);
-
-        // fetch(url)
-        // .then((response) => response.json())
-        // .then((data) => 
-    
-        //     document.getElementsByClassName('js-item')[4].innerHTML = `
-        //     <div class="img-wrapper"></div>
-        //     <div>
-        //         <h3 class="grid-txt">${data.articles[4].title}</h3>
-        //         <div><a href="${data.articles[4].url}" class="btn-sec">Read More</a></div>
-        //     </div>
-        //     `);
-
-        // fetch(url)
-        // .then((response) => response.json())
-        // .then((data) => 
-    
-        //     document.getElementsByClassName('js-item')[5].innerHTML = `
-        //     <div class="img-wrapper"></div>
-        //     <div>
-        //         <h3 class="grid-txt">${data.articles[5].title}</h3>
-        //         <div><a href="${data.articles[5].url}" class="btn-sec">Read More</a></div>
-        //     </div>
-        //     `);
-
+        `
+        }))
 
 
