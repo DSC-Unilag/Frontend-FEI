03d8610ef7432acf5f4f23888da06d293ac57071
Mikaelson1101 <eugenemichael1999@gmail.com>
2018-12-10 09:35:08 +0100

    Nav-bar Optimized for Mobile Only --Need to fix desktop view

--- michael-eugene/assets/js/script.js
+++ michael-eugene/assets/js/script.js
@@ -1,17 +1,47 @@
-// Navigation Bar
-const navToggle = document.querySelector('.menu-icon')
-
-navToggle.addEventListener("click", () => {
-    document.body.classList.toggle("nav-is-visible")
-})
+// If you are reading this, im new to javascript so most of my code will be irrelevant.... lol
+// Please feel free to help refactor my code... I would greatly appreciate it ;) Cheers
 
 
-var numbers_ = [1, 2, 3, 4, 5];
-var text = "";
-var i;
-for (i = 0; i < numbers_.length; i++) { 
-  text += numbers_[i] - 1
-}
+
+
+
+
+
+
+// Navigation Scripting Begins
+
+    // Navigation Slide Out Menu
+    const navToggle = document.querySelector('.menu-icon')
+
+    navToggle.addEventListener("click", () => {
+        document.body.classList.toggle("nav-is-visible");
+    })
+
+    // Navigation Item Clicked Behaviour
+    var navItem = document.querySelectorAll('.nav-link')[0];
+    var navItem1 = document.querySelectorAll('.nav-link')[1];
+    var navItem2 = document.querySelectorAll('.nav-link')[2];
+    var navItem3 = document.querySelectorAll('.nav-link')[3];
+    var navItem4 = document.querySelectorAll('.nav-link')[4];
+
+
+    navItem.addEventListener("click", () => {
+        document.body.classList.remove("nav-is-visible");
+    })
+    navItem1.addEventListener("click", () => {
+        document.body.classList.remove("nav-is-visible");
+    })
+    navItem2.addEventListener("click", () => {
+        document.body.classList.remove("nav-is-visible");
+    })
+    navItem3.addEventListener("click", () => {
+        document.body.classList.remove("nav-is-visible");
+    })
+    navItem4.addEventListener("click", () => {
+        document.body.classList.remove("nav-is-visible");
+    })
+
+// Navigation SCripting Ends.....
 
 
 // BBC API
