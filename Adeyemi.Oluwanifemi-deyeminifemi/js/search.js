1cb06cfa9bed8b200b209c4ac7a51f5b08119263
Nifemi <deyeminifemi@gmail.com>
2018-12-10 22:00:19 +0100

    Created search page and implemented search mechanism

---
+++ Adeyemi.Oluwanifemi-deyeminifemi/js/search.js
@@ -0,0 +1,41 @@
+const searchQuery = location.search.split('=')[1];
+let request = `https://newsapi.org/v2/top-headlines?apiKey=9ac2b559698d40bc9757fb14d7a6925c&q=${searchQuery}`;
+const search = async (request) => {
+    const reply = await fetch(request);
+    const replyjson = await reply.json();
+    return replyjson;
+}
+
+search(request).then((data) => {
+    data.articles.forEach((elem) => {
+        if(elem.title && elem.description && elem.publishedAt){
+            const title = elem.title;
+            const desc = elem.description;
+            const date = new Date(elem.publishedAt);
+            const datetxt = `${date.getDay()}/${date.getMonth()}/${date.getYear()}`
+            const li = document.createElement('li');
+            const link = document.createElement('a');
+            link.src = elem.url;
+            const titleElem = document.createElement('p');
+            titleElem.textContent = title;
+            titleElem.classList.add('title')    
+            const descElem = document.createElement('p');
+            descElem.textContent = desc;
+            descElem.classList.add('text-muted')
+            const dateElem = document.createElement('span');
+            dateElem.textContent = datetxt;
+            dateElem.classList.add('date')    
+            link.appendChild(titleElem)
+            link.appendChild(descElem)
+            link.appendChild(dateElem)
+            li.appendChild(link);
+            document.querySelector('#results').appendChild(li)
+                
+            
+        }
+    })
+    
+    
+})
+
+
