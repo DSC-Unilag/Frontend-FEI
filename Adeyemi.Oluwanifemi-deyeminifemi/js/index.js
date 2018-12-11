65829c4b4cc79589769451667aecd3d687c28c71
Nifemi <deyeminifemi@gmail.com>
2018-12-11 21:24:42 +0100

    Added latest stories functionality

--- Adeyemi.Oluwanifemi-deyeminifemi/js/index.js
+++ Adeyemi.Oluwanifemi-deyeminifemi/js/index.js
@@ -7,6 +7,14 @@ const getStories = async (args,type = "top") => {
     return await response.json();
 }
 
+const getlastestStories = async () => {
+    const now = new Date;
+    let from = "2018-11-12T";
+    from += `${now.getHours()}:${now.getMinutes()-30}:00`
+    let request = `https://newsapi.org/v2/everything?apiKey=9ac2b559698d40bc9757fb14d7a6925c&language=en&from=${from}&domains=washingtontimes.com,domain=bbc.co.uk,bleacherreport.com,businessinsider.com,dailymail.co.uk,espn.com,mashable.com,mtv.com,talksport.com,techradar.com,nytimes.com`;
+    const response = await fetch(request);
+    return await response.json();
+}
 const displayTopStories = getStories({country : "us",limit : 10})
 
 displayTopStories.then((data)=>{
@@ -15,6 +23,7 @@ displayTopStories.then((data)=>{
     for(let i = 0;created < 6;i++){
         const article = articles[i];
         if(article.title && article.url && article.urlToImage && article.content){
+            document.querySelector(`#top-card-${created+1} .loader`).style.display = "none";
             document.querySelector(`#top-card-${created+1}`).appendChild(createCard(article));
             created++;
         }
@@ -28,6 +37,7 @@ displayPolitics.then((data) => {
     for(let i = 0;created < 4 && i < data.totalResults;i++){
         const article = articles[created];
         if(article.title && article.url && article.urlToImage && article.content){
+            document.querySelector(`#politics-card-${created+1} .loader`).style.display = "none";
             document.querySelector(`#politics-card-${created+1}`).appendChild(createCard(article));
             created++;
         }
@@ -41,11 +51,31 @@ displaySports.then((data) => {
     for(let i = 0;created < 4 && i < data.totalResults;i++){
         const article = articles[created];
         if(article.title && article.url && article.urlToImage && article.content){
+            document.querySelector(`#sports-card-${created+1} .loader`).style.display = "none";
             document.querySelector(`#sports-card-${created+1}`).appendChild(createCard(article));
             created++;
         }
     }
 })
+
+const displayLatestStories = async () => {
+    const latestStories = await getlastestStories();
+    for(let i = 0;i < latestStories.articles.length && i < 20;i++){
+        const article = latestStories.articles[i];
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        const link = document.createElement('a');
+        link.href = article.url;
+        span.classList.add('text-muted');
+        const date = new Date(article.publishedAt);
+        span.textContent = `${date.getHours() % 12}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}`;
+        link.appendChild(span);
+        link.innerHTML += article.title;
+        li.appendChild(link)
+        document.querySelector('#latest-stories ul').appendChild(li);
+    } 
+} 
+displayLatestStories();
 
 const createCard = (data) => {
     const card = document.createElement('div');
