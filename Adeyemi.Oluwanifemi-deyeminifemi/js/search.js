587ccdda74effa0150ac59941cc89e4997e6b21b
Nifemi <deyeminifemi@gmail.com>
2018-12-13 15:24:04 +0100

    Added responsitivity

--- Adeyemi.Oluwanifemi-deyeminifemi/js/search.js
+++ Adeyemi.Oluwanifemi-deyeminifemi/js/search.js
@@ -8,19 +8,24 @@ document.querySelector('#next-page').value = parseInt(page) + 1;
 if(page === 10){
     document.querySelector('#next-page').style.display = "none";
 }
+let display = 0;
 let request = `https://newsapi.org/v2/everything?apiKey=9ac2b559698d40bc9757fb14d7a6925c&q=${searchQuery}&language=en`;
 request += page !== 1 ? `&page=${page}` : page;
 const search = async (request,args = {}) => {
-    args.sortBy = args.sortBy ? args.sortBy : "publishedAt";
-    request += `&sortBy=${args.sortBy}`;
-    const reply = await fetch(request);
-    const replyjson = await reply.json();
-    return replyjson;
+    try{
+        args.sortBy = args.sortBy ? args.sortBy : "publishedAt";
+        request += `&sortBy=${args.sortBy}`;
+        const reply = await fetch(request);
+        const replyjson = await reply.json();
+        return replyjson;
+    }catch(err){
+        return undefined;
+    }
 }
 
 
 const processData = (data) => {
-    if(data.totalResults){
+    if(data && data.totalResults){
         const totalPages = parseInt(data.totalResults / 20);
         for(let i = totalPages + 1;i <= 10;i++){
             document.querySelector(`#page-${i}`).style.display = "none";
@@ -54,7 +59,9 @@ const processData = (data) => {
     document.querySelector('#results').style.display = "block";
     document.querySelector('#links').style.display = "block";
     }else{
-        document.querySelector('')
+        document.querySelector('#loader').style.display = "none";        
+        document.querySelector('#errors').style.display = "block";        
+        document.querySelector('#errors').innerHTML = data ?  "There were no results related to your search" : "You seem to be offline. Please connect to the internet and try again";
     }
 }
 
@@ -64,6 +71,7 @@ search(request).then(processData)
 
 document.querySelector('#filter select').addEventListener('change',(e) => {
     document.querySelector('#loader').style.display = "block";
+    document.querySelector('#errors').style.display = "none";
     document.querySelector('#results').style.display = "none";
     document.querySelector('#links').style.display = "none";
     document.querySelector('#results').innerHTML = '';
@@ -72,6 +80,7 @@ document.querySelector('#filter select').addEventListener('change',(e) => {
 
 document.querySelector('#search-lg button').addEventListener('click',(e) => {
     document.querySelector('#loader').style.display = "block";
+    document.querySelector('#errors').style.display = "none";    
     document.querySelector('#results').style.display = "none";
     document.querySelector('#links').style.display = "none";
     document.querySelector('#results').innerHTML = '';
@@ -81,4 +90,20 @@ document.querySelector('#search-lg button').addEventListener('click',(e) => {
 })
 
 
+// Code for responsive navbar
+window.addEventListener('resize',(e) => {
+    
+    if(window.innerWidth > 700){
+        document.querySelector('nav').classList.remove('active');
+    }
+})
 
+document.querySelector('#navsmallbtn').addEventListener('click', () => {
+    if(display){
+        document.querySelector('nav').classList.remove('active');
+        display--;
+    }else{
+        document.querySelector('nav').classList.add('active');
+        display++;
+    }
+})
