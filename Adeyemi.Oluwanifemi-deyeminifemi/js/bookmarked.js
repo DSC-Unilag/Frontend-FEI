b87e1c79646c540789d8f4c79b4bf3f0ff07be3d
Nifemi <deyeminifemi@gmail.com>
2018-12-18 22:51:33 +0100

    Fixed up js Responsive Nav Bookmark toggling and what not

--- Adeyemi.Oluwanifemi-deyeminifemi/js/bookmarked.js
+++ Adeyemi.Oluwanifemi-deyeminifemi/js/bookmarked.js
@@ -1,8 +1,37 @@
 const articles = localStorage.getItem("bookmarked") ? JSON.parse(localStorage.getItem("bookmarked")) : [];
 if(articles.length){
-    articles.forEach((e) => {
-        document.querySelector("#stories").appendChild(createCard(e))
+    articles.forEach((e,index) => {
+        document.querySelector("#stories").appendChild(createCard(e,e.id,"bookmarked"))
     })
 }else{
-    document.querySelector('div.error').style.display = "block"
+    document.querySelector('div.no_bookmark').style.display = "block"
 }
+if(!(navigator.onLine)){
+    document.querySelector('div.offline').style.display = "block";
+}
+// Code for responsive navbar
+window.addEventListener('resize',(e) => {    
+    if(window.innerWidth > 700){
+        document.querySelector('nav').classList.remove('active');
+    }
+})
+let display = 0;
+document.querySelector('#navsmallbtn').addEventListener('click', () => {
+    if(display){
+        document.querySelector('nav').classList.remove('active');
+        display--;
+    }else{
+        document.querySelector('nav').classList.add('active');
+        display++;
+    }
+})
+document.querySelectorAll('.bookmark').forEach((e) => {
+    e.classList.add('active')
+})
+document.querySelectorAll('.bookmark').forEach((e) => {
+    e.addEventListener('click',() => {
+        const active = toggleClass(e,'active');
+        active ? addBookmark(e.id) : removeBookmark(e.id);
+        location.reload();
+    })
+})
