d75ca2799d3e33cea21ee663ca696f8511a12f9c
prince-curie <prince.chibuike@gmail.com>
2018-12-20 21:11:32 +0100

    functionality for fetching news by category

--- Onwubiko-Chibuike/main.js
+++ Onwubiko-Chibuike/main.js
@@ -2,11 +2,25 @@ let apikeys = '7293043e37d649e28cd3848e276a7d3e';
 let url = 'https://newsapi.org/v2/top-headlines?' +
             'country=ng&'+
             'apiKey='+apikeys;
+let sportsUrl = 'https://newsapi.org/v2/top-headlines?'
+                + 'country=ng&'
+                + 'category=sports&'
+                + 'apiKey='+apikeys;
+let technologyUrl = 'https://newsapi.org/v2/top-headlines?'
+                    + 'country=ng&'
+                    + 'category=technology&'
+                    + 'apiKey='+apikeys;
+
 let req = new Request(url);
+let sportsRequest = new Request(sportsUrl);
+let technologyRequest = new Request(technologyUrl);
 
 let input = document.querySelectorAll('.news');
-let image = document.querySelectorAll('img');
-
+let topNewsImages = document.querySelectorAll('.top-news-images');
+let sportsNews = document.querySelectorAll('.sport-news');
+let sportsNewsImages = document.querySelectorAll('.sport-news-img');
+let techNews = document.querySelectorAll('.tech-news');
+let techNewsImages = document.querySelectorAll('.technology-news-image');
 let searchHeader = document.createElement('h3');
 
 let search = document.getElementById("search");
@@ -18,13 +32,34 @@ function validateAndReturnResponse(res) {
     return res.json();
 }
 
+function displaySportsOutput(output) {
+    for (let i = 0; i <= sportsNews.length-1; i++) {
+        sportsNews[i].textContent = output.articles[i].title;
+        sportsNews[i].href = output.articles[i].url;
+        sportsNewsImages[i].src = output.articles[i].urlToImage;
+        //console.log(output.articles[i]);
+    }
+    return;
+}
+
 function displayOutput(output) {
-    for (let i = 0; i < input.length; i++) {
+    for (let i = 0; i <= input.length-1; i++) {
         input[i].textContent = output.articles[i].title;
         input[i].href = output.articles[i].url;
-        image[i].src = output.articles[i].urlToImage;
-        console.log(output.articles[i]);
+        topNewsImages[i].src = output.articles[i].urlToImage;
+        //console.log(output.articles[i]);
     }
+    return;
+}
+
+function displayTechnologyOutput(output) {
+    for (let i = 0; i <= techNews.length-1; i++) {
+        techNews[i].textContent = output.articles[i].title;
+        techNews[i].href = output.articles[i].url;
+        techNewsImages[i].src = output.articles[i].urlToImage;
+        //console.log(output.articles[i]);
+    }
+    return;
 }
 
 function displaySearchOutput(output) {
@@ -47,7 +82,7 @@ function displaySearchOutput(output) {
             searchResultLink.textContent = output.articles[i].title;
             searchResultLink.href = output.articles[i].url;
             searchResultContent.textContent = output.articles[i].description;
-            console.log(output.articles[i],  searchResultLink.href, searchResultContent.textContent );
+            console.log(output.articles[i]);
             document.querySelector('main').appendChild(searchResult);
         }
     }
@@ -58,14 +93,16 @@ function catchError(error) {
     console.log('looks like there was a problem: \n' + error );
 }
 
-function makeRequest(req) {
+function makeRequest(req,displayOutput) {
     fetch(req)
     .then(validateAndReturnResponse)
     .then(displayOutput)
     .catch(catchError)
 }
 
-makeRequest(req);
+makeRequest(req, displayOutput);
+makeRequest(sportsRequest, displaySportsOutput);
+makeRequest(technologyRequest, displayTechnologyOutput);
 
 //Search scipt
 
@@ -104,15 +141,12 @@ function makeSearchRequest(e) {
 
     let init = {
         method: 'GET',
-        headers: {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"},
+        headers: {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"}
     };
 
     let request = new Request(searchUrl, init);
     
-    fetch(request)
-    .then(validateAndReturnResponse)
-    .then(displaySearchOutput)
-    .catch(catchError)
+    makeRequest(request, displaySearchOutput);
     return;
 }
 
