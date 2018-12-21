e3fae35cde24358cad139f0f694c3b178e4cd3ce
Nifemi <deyeminifemi@gmail.com>
2018-12-21 05:48:22 +0100

    Added sw caching and loading from cache

--- Adeyemi.Oluwanifemi-deyeminifemi/sw.js
+++ Adeyemi.Oluwanifemi-deyeminifemi/sw.js
@@ -1,8 +1,31 @@
+self.addEventListener('install',(e) => {
+    e.waitUntil(
+        caches.open('default')
+        .then((cache) => {
+            console.log('[Service-Worker] Precaching App Shell')
+            cache.addAll([
+                '/Adeyemi.Oluwanifemi-deyeminifemi/',
+                'index.html',
+                'images/favicon.ico',
+                'css/index.css',
+                'images/svg/search.svg',
+                'images/svg/navsmall.svg',
+                'js/functions.js',
+                'js/index.js',
+                'manifest.json'
+            ])
+        })
+    )
+})
+
 self.addEventListener('activate', () => {
     return self.clients.claim();
 })
 
 self.addEventListener('fetch',(event) => {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response ? response : fetch(event.response))
+    );
 })
 
